197d89b0f0dbea40e0f8900627df3245c754903a
ahmad.mardiana <ahmad.mardiana@ifg-life.id>
2023-05-03 14:55:22 +0700

    delete json tag

--- model/Article.go
+++ model/Article.go
@@ -3,8 +3,8 @@ package model
 import "time"
 
 type Article struct {
-	Id        string    `json:"id" bson:"id"`
-	Title     string    `json:"title" bson:"title"`
-	CreatedAt time.Time `json:"createdAt" bson:"createdAt"`
-	Content   string    `json:"content" bson:"content"`
+	Id        string    `bson:"id"`
+	Title     string    `bson:"title"`
+	CreatedAt time.Time `bson:"createdAt"`
+	Content   string    `bson:"content"`
 }
